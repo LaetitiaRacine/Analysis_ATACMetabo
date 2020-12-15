4ddf7558268b685859ab0c80331ac30da8d29032
Laëtitia Racine <laetitia.racine@hotmail.fr>
2020-12-15 18:39:52 +0100

    Histogram nbreads.peak & threshold
    Script peaks_featureCounts.R modifié, fonctionne pour créer fichier .csv avec "cond, time, donor, peak, nbreads" pour pouvoir faire les graphs. Option output .csv ajoutée.  Rajout du séparateur "," à l'enregistrement pour que tous les csv aient le même.
    Script plot_QC_variation modifié : ajout du code pour créer les graphiques nbreads/peak et marque du threshold. Output en pdf. 
    Script peak_filter.R créé : script permettant d'appliquer le threshold sur les fichiers .broadPeak pour ne garder que ceux qui ont nbreads >=10
    Environnement modifié : ajout de tidyR et ggforce
    Snakefile modifié : rule readcount_matrix ok (permet de générer un fichier csv avec nbreads/peak à partir d'un grange), rule qc_peaks_threshold ok (permet de générer les fichiers .threshold.broadPeak), rule nbpeaks_report modifiée ok (ajout des colonnes nbpeaks after threshold et pourcentage de perte), rule nbreads_per_peak_report ok (fichier csv global fusionnant tous les readcounts), rule plot_reports modifiée (création graphiques nb peaks et nbread/peak, nom enregistrement des output à vérifier).  Toutes ces rules ont été testées dans le nouvel environnement et fonctionnent. 
    
    A faire :
    - problème avec le facet_wrap des nbreads/peak 
    - voir ajustement des légendes dans graphiques 
    - renommer le script plot_QC_variation ?

--- C_scripts/peaks_featureCounts.R
+++ C_scripts/peaks_featureCounts.R
@@ -9,21 +9,28 @@ Usage:
 Options:
   --output_rds <file>  Output rds file
   --output_txt <file>  Output matrix count txt file
+  --output_csv <file>  Output csv file
   -h, --help           Show this screen
 " -> doc
 
 library(docopt)
 arguments <- docopt(doc)
 
-suppressPackageStartupMessages(suppressWarnings(library(Rsubread))
-                               
-peaks_gr_to_df = function(gr) {
+suppressPackageStartupMessages({
+  suppressWarnings(library(GenomicRanges))
+  suppressWarnings(library(Rsubread))
+  suppressWarnings(library(dplyr))
+})
+
+                 
+peaks_gr_to_df = function(peaks_gr) {
   peaks_df = data.frame(peaks_gr, stringsAsFactors = FALSE)
   peaks_df$seqnames = as.character(peaks_df$seqnames) # enlever le factor
   peaks_df$strand = as.character(peaks_df$strand)     # enlever le factor
-  peaks_df$name = paste0("peak_",rownames(peaks_df))  # ajout colonne nom du peak
-  peaks_df <- peaks_df[,c("name","seqnames","start","end","strand")] # réorganise l'ordre des colonnes
-  colnames(peaks_df) <- c("GeneID","Chr","Start","End","Strand") # renomme les colonnes
+  ranges_df = data.frame(ranges(peaks_gr)) # récuperer le nom des peaks
+  peaks_df = full_join(ranges_df, peaks_df, by = c("start","end","width"))
+  peaks_df <- peaks_df[,c("names","seqnames","start","end","strand")] # réorganise l'ordre des colonnes
+  colnames(peaks_df) = c("GeneID","Chr","Start","End","Strand")
   return(peaks_df)
 }
 
@@ -31,8 +38,9 @@ peaks_gr = readRDS(arguments$peaks_gr_input)
 
 peaks_df = peaks_gr_to_df(peaks_gr)
 
+
 readCount <- featureCounts(files = arguments$bam_input,
-                           annot.ext = peaks_df,
+                           annot.ext = peaks_df,         # Besoin d'avoir les noms GeneID, Chr, Start, End et Strand pour fonctionner
                            isPairedEnd = TRUE,
                            nthreads = 1,
                            countChimericFragments = FALSE,
@@ -49,3 +57,14 @@ if (!is.null(arguments$output_txt)) {
   matrix_count = readCount$counts
   write.table(matrix_count, file = arguments$output_txt, sep = "\t", quote = FALSE)
 }
+
+if (!is.null(arguments$output_csv)) {
+  print("Save csv...")
+  count_df = tibble::rownames_to_column(data.frame(readCount$counts), "name")
+  count_df = tidyr::separate(count_df, col = name, into = c("condition","time","donor", "peak", "num"), sep="_", remove = TRUE) 
+  count_df = tidyr::unite(count_df, col = peakID, peak, num, sep="_", remove = TRUE)
+  colnames(count_df) = c("condition","time","donor","peakID","nbreads")
+  write.table(count_df, file = arguments$output_csv, sep = ",", row.names = FALSE) 
+  # write.csv2(count_df, file = arguments$output_csv, sep = ",", row.names = FALSE) 
+}
+
---
+++ C_scripts/peaks_filter.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+#########################
+### Command line to call the script in linux consol
+#########################
+
+"Filter broad peak to keep peaks that passed threshold value
+
+Usage:
+  peaks_filter.R [options] <broadPeak_input> <csv_input> <output>
+  peaks_filter.R -h | --help
+
+Options:
+  -h, --help           Show this screen
+" -> doc
+
+
+library(docopt)
+arguments <- docopt(doc)
+
+
+#########################
+### Libraries and input loading
+#########################
+
+suppressPackageStartupMessages(suppressWarnings(library(dplyr)))
+
+peaks_bP = read.table(arguments$broadPeak_input, stringsAsFactors = FALSE)                   # load .broadPeak file
+peaks_readcount = read.csv2(arguments$csv_input, sep = ";")                                  # load .csv readcount matrix file
+
+
+########################
+### Script
+########################
+
+peaks_to_keep = peaks_readcount  %>% 
+  dplyr::filter(nbreads>=10) %>%                                                             # threshold application, suppress peaks that don't pass nbreads >= 10
+  dplyr::mutate(peak_threshold_passed = paste(condition,time,donor,peakID,sep="_")) %>%      # new column to have same peak name as in broadPeak file
+  dplyr::select(peak_threshold_passed)                                                       # keep only peaks name
+peaks_to_keep = peaks_to_keep[[1]]                                                           # transform into character vector
+
+peaks_bP_threshold = peaks_bP %>%                                                            
+  dplyr::filter(V4 %in% peaks_to_keep)                                                       # keep lines from original broadPeak that passed the threshold
+
+print("Save .threshold.broadPeak file")  
+write.table(peaks_bP_threshold, file = arguments$output, sep = ";", quote = FALSE)           # save threshold.broadPeak file
+
