46253dc212c9df2838be0e0d210fd915b8819e8a
Laëtitia Racine <laetitia.racine@hotmail.fr>
2021-01-18 10:33:07 +0100

    Correct plot_QC_variation error
    Le script présentait une erreur quand on change le nombre d'échantillon pour le dernier plot read_graph. Le nombre de pages indiqué pour la boucle n'était pas adapté entraînant une erreur. On utilise la fonction n_pages pour déterminer le nombre de pages à utiliser. 
    Test snakefile sans point 06h : le snakefile fonctionne en entier sans erreur.

--- C_scripts/plot_QC_variation.R
+++ C_scripts/plot_QC_variation.R
@@ -140,29 +140,51 @@ df_report = df_report %>%
   dplyr::mutate(condition_time_donor = paste(condition, time, donor, sep="_")) %>%
   dplyr::select(-time, -donor, -peakID) 
 
-var_x = as.integer(df_report$nbreads)
+var_x = as.numeric(as.character(df_report$nbreads))
 var_ylegend = "Number of peaks"
 
+# Préimpression du graphique pour connaître le nombre de pages à utiliser dans la boucle
+preprint_plot = ggplot(df_report, aes(x=var_x, fill = condition)) +
+  geom_histogram(color = "black", position = position_dodge2(width = 0.8, preserve = "single"))+
+  geom_vline(xintercept = 10, color ="red", linetype = "dashed", size = 1)+ 
+  theme(legend.position = "right",
+        plot.title = element_text(hjust = 0.5, face = "bold", colour= "black", size = 16),
+        strip.text.x = element_text(size=11, color="black", face="bold.italic"),
+        axis.line.y = element_line(colour = "black"),
+        axis.text.y = element_text(size = 11, colour = "black"),
+        axis.ticks.y = element_line() ,
+        axis.title.y = element_text(vjust = 1 ,size = 14),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(vjust = -0.5, size = 11, colour = "black"),
+        axis.ticks = element_blank()) +
+  xlim(c(0,200)) +
+  facet_wrap_paginate(condition_time_donor~., scales = "free_x", ncol = 4, nrow = 4, page = 1) +
+  labs(y = var_ylegend) 
+nb_pages = n_pages(preprint_plot)
+
+# Création du vrai graphique
 pdf(width = 16*0.75, height = 9*0.75, file = arguments$output)
-for(i in 1:4) {
+for(i in 1:nb_pages) {
   print(ggplot(df_report, aes(x=var_x, fill = condition)) +
-          geom_histogram(color = "black", position = position_dodge2(width = 0.8, preserve = "single"))+
-          geom_vline(xintercept = 10, color ="red", linetype = "dashed", size = 1)+ 
-          theme(legend.position = "right",
-                plot.title = element_text(hjust = 0.5, face = "bold", colour= "black", size = 16),
-                strip.text.x = element_text(size=11, color="black", face="bold.italic"),
-                axis.line.y = element_line(colour = "black"),
-                axis.text.y = element_text(size = 11, colour = "black"),
-                axis.ticks.y = element_line() ,
-                axis.title.y = element_text(vjust = 1 ,size = 14),
-                axis.title.x = element_blank(),
-                axis.text.x = element_text(vjust = -0.5, size = 11, colour = "black"),
-                axis.ticks = element_blank()) +
-          xlim(c(0,200)) +
-          facet_wrap_paginate(condition_time_donor~., scales = "free_x", ncol = 4, nrow = 4, page = i) +
-          labs(y = var_ylegend) 
-        )
-    }
+        geom_histogram(color = "black", position = position_dodge2(width = 0.8, preserve = "single"))+
+        geom_vline(xintercept = 10, color ="red", linetype = "dashed", size = 1)+ 
+        theme(legend.position = "right",
+              plot.title = element_text(hjust = 0.5, face = "bold", colour= "black", size = 16),
+              strip.text.x = element_text(size=11, color="black", face="bold.italic"),
+              axis.line.y = element_line(colour = "black"),
+              axis.text.y = element_text(size = 11, colour = "black"),
+              axis.ticks.y = element_line() ,
+              axis.title.y = element_text(vjust = 1 ,size = 14),
+              axis.title.x = element_blank(),
+              axis.text.x = element_text(vjust = -0.5, size = 11, colour = "black"),
+              axis.ticks = element_blank()) +
+        xlim(c(0,200)) +
+        facet_wrap_paginate(condition_time_donor~., scales = "free_x", ncol = 4, nrow = 4, page = i) +
+        labs(y = var_ylegend) 
+      )
+  }
 dev.off()
 
 }
+
+
